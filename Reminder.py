5a76ca1b6725b34c1bc2aa4578d1edca7f922ecd
Rohan sengupta <64458868+RohanSengupta326@users.noreply.github.com>
2020-07-28 19:27:21 +0530

    Update Reminder.py

--- Reminder.py
+++ Reminder.py
@@ -1,11 +1,40 @@
-import time 
-from plyer import notification
+import sys
+import string
+from time import sleep
+import time
+    
+tm = time.asctime()
+var = tm.split()    
+print var
 
-if __name__ == '__main__':
-    while True:
-        notification.notify(
-            title = "Bullshit Reminder",
-            message = "I have nothing to say , just a bullshit reminder program",
-            timeout=10
-        )
-        time.sleep(60*60)
+
+try:
+    #minutes = int(raw_input("Enter the number of minutes?? "))
+    minutes = int(raw_input("Enter the number of minutes "))
+except ValueError:
+    print "Invalid numeric value (%s) for minutes" % minutes
+    print "Should be an integer >= 0"
+    sys.exit(1)
+
+if minutes < 0:
+    print "Invalid value for minutes, should be >= 0"
+    sys.exit(1)
+
+seconds = minutes * 60
+
+if minutes == 1:
+    unit_word = " minute"
+else:
+    unit_word = " minutes"
+
+try:
+    if minutes > 0:
+        print "Sleeping for " + str(minutes) + unit_word
+        sleep(seconds)
+    print "Wake up"
+    for i in range(5):
+        print chr(7),
+        sleep(1)
+except KeyboardInterrupt:
+    print "Interrupted by user"
+    sys.exit(1)
